65f15f91d1dc0c5be2597ae424509083a772a2d0
Lothobah <66518062+Lothobah@users.noreply.github.com>
2021-11-12 19:22:53 +0000

    Commit

--- StudentManagementSystem/settings.py
+++ StudentManagementSystem/settings.py
@@ -83,13 +83,20 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'student_management_system',
-        'USER': 'student_management_system2',
+        'USER': 'kingloth-koo',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306'
     }
 }
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
